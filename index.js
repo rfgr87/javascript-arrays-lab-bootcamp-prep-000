fd8738ef69b7f786939743686ba943a364abd127
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-02-05 23:45:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,32 @@ function destructivelyAppendKitten(name){
   var newarray = kittens.push(name)
   return newarray
 }
+
+function destructivelyPrependKitten(name){
+  var newarray = kittens.unshift(name)
+  return newarray
+}
+
+function destructivelyRemoveLastKitten(){
+  var newarray = kittens.pop()
+  return newarray
+}
+
+function destructivelyRemoveFirstKitten(){
+  var newarray = kittens.shift()
+  return newarray
+}
+
+function appendKitten(name){
+  var newarray = kittens + name
+  return newarray
+}
+
+function prependKitten(name){
+  var newarray = [name + kittens]
+  return newarray
+}
+
+function removeLastKitten(){
+  var newarray = kittens.
+}
